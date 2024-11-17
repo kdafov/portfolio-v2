95a05614692c2e4153ac3c4afca26217e9bfb10d
forfail <business.dafov@gmail.com>
2024-11-17 22:02:31 +0000

    Fix tailwind bug with styling class

--- src/_components/BentoSubGrid.tsx
+++ src/_components/BentoSubGrid.tsx
@@ -25,10 +25,28 @@ export const BentoSubGrid = ({
     first,
     last,
 }: BentoSubGridProps): ReactElement => {
+    const colSpanClassMap: { [key: number]: string } = {
+        1: 'lg:col-span-1',
+        2: 'lg:col-span-2',
+        3: 'lg:col-span-3',
+        4: 'lg:col-span-4',
+        5: 'lg:col-span-5',
+        6: 'lg:col-span-6',
+    };
+
+    const positionClassMap: { [key: string]: string } = {
+        tl: 'rounded-tl-2xl',
+        tr: 'rounded-tr-2xl',
+        bl: 'rounded-bl-2xl',
+        br: 'rounded-br-2xl',
+    };
+
     return (
-        <div className={`flex p-px lg:col-span-${colSpan} sm:block lg:flex`}>
+        <div className={`flex p-px sm:block lg:flex ${colSpanClassMap[colSpan]}`}>
             <div
-                className={`overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-${position}-[2rem] 
+                className={`overflow-hidden bg-gray-800 ring-1 ring-white/15 ${
+                    positionClassMap[position]
+                } 
                     ${first ? 'max-lg:rounded-t-[2rem]' : ''} 
                     ${last ? 'max-lg:rounded-b-[2rem]' : ''} 
                     ${children ? 'w-full flex items-center p-8' : ''}
