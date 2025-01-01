724be2ed40e9ef7ef8d5a3859ea3d47979f48f25
forfail <business.dafov@gmail.com>
2025-01-01 21:49:11 +0000

    Fixed copyright year

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -6,7 +6,8 @@ export default function Footer(): ReactElement {
             <div className="mx-auto max-w-7xl px-6 py-4 lg:px-8">
                 <div className="border-t border-gray-200 pt-4">
                     <p className="text-sm text-gray-600 text-center">
-                        &copy; 2024 Kristian Dafov, KD Inc. All rights reserved.
+                        &copy; 2024-{new Date().getFullYear()} Kristian Dafov, KD Inc. All rights
+                        reserved.
                     </p>
                 </div>
             </div>
